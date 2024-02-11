d9fb022879303971d64a6d20eb80eaf342a4a97a
yyuso <132357180+yyuso@users.noreply.github.com>
2024-02-11 19:39:28 +0300

    nest

--- assets/script/js/script.js
+++ assets/script/js/script.js
@@ -7,3 +7,22 @@ var mainSlider = new Swiper(".mainslider", {
     el: ".swiper-pagination",
   },
 });
+
+var productsSlider = new Swiper(".productsSlider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 10,
+      spaceBetween: 2
+    },
+    400: {
+      slidesPerView: 10,
+      spaceBetween: 2
+    },
+  }
+});
