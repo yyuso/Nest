93fa10ddba1c876f9e5de1e8d04ad776789b65cd
yyuso <132357180+yyuso@users.noreply.github.com>
2024-02-21 21:55:10 +0300

    nest

--- assets/script/js/script.js
+++ assets/script/js/script.js
@@ -46,30 +46,63 @@ var swiper = new Swiper(".mydailybestsells", {
   }
 });
 
-
 var lastClickedElement = document.querySelector('.clicked');
+function openCity(navsName, clickedElement) {
+  // Eğer bir linkse tıklandığında sayfa başına atlamayı engelle
+  event.preventDefault();
 
-    function openCity(navsName, clickedElement) {
-      // Eğer bir linkse tıklandığında sayfa başına atlamayı engelle
-      event.preventDefault();
+  // Eski tıklanan öğenin rengini sıfırla
+  if (lastClickedElement !== null) {
+    lastClickedElement.classList.remove("clicked");
+  }
 
-      // Eski tıklanan öğenin rengini sıfırla
-      if (lastClickedElement !== null) {
-        lastClickedElement.classList.remove("clicked");
-      }
+  // Yeni tıklanan öğenin rengini değiştir
+  clickedElement.classList.add("clicked");
 
-      // Yeni tıklanan öğenin rengini değiştir
-      clickedElement.classList.add("clicked");
+  // LastClickedElement'i güncelle
+  lastClickedElement = clickedElement;
 
-      // LastClickedElement'i güncelle
-      lastClickedElement = clickedElement;
+  // Diğer tablardaki içerikleri gizle
+  var tabs = document.getElementsByClassName("tabs");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
 
-      // Diğer tablardaki içerikleri gizle
-      var tabs = document.getElementsByClassName("tabs");
-      for (var i = 0; i < tabs.length; i++) {
-        tabs[i].style.display = "none";
-      }
+  // Belirtilen tabı göster
+  document.getElementById(navsName).style.display = "block";
+}
 
-      // Belirtilen tabı göster
-      document.getElementById(navsName).style.display = "block";
-    }
+
+function popup() {
+  var div = document.getElementById("PopupMenu");
+  if (div.classList.contains("active")) {
+      div.classList.remove("active");
+     var pass_icon = document.getElementsByClassName("fa-circle-xmark")[0];
+      pass_icon.classList.remove("fa-circle-xmark");
+      pass_icon.classList.add("fa-bars");
+  } else {
+      div.classList.add("active");
+      
+       var pass_icon = document.getElementsByClassName("fa-bars")[0];
+      pass_icon.classList.remove("fa-bars");
+      pass_icon.classList.add("fa-circle-xmark");
+  }
+}
+
+    // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
