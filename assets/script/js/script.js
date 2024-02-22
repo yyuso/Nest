b7807c57e733898b4fa7fc717ab4c0810a4d9a22
Yusuf Baran <132357180+yyuso@users.noreply.github.com>
2024-02-22 13:31:26 +0300

    Update script.js

--- assets/script/js/script.js
+++ assets/script/js/script.js
@@ -120,7 +120,7 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("stickyv2")
   } else {
     navbar.classList.remove("sticky");
   }
