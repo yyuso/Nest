b1e13dd78119f0983b3491b6feb405f48683aad0
Yusuf Baran <132357180+yyuso@users.noreply.github.com>
2024-02-22 14:23:18 +0300

    Update script.js

--- assets/script/js/script.js
+++ assets/script/js/script.js
@@ -113,14 +113,14 @@ window.onscroll = function() {myFunctionv2()};
 var navbar1 = document.getElementById("navbarv2");
 
 // Get the offset position of the navbar
-var sticky1 = navbar.offsetTop;
+var sticky1 = navbar1.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunctionv2() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("stickyv2")
+  if (window.pageYOffset >= sticky1) {
+    navbar1.classList.add("stickyv2")
   } else {
-    navbar.classList.remove("stickyv2");
+    navbar1.classList.remove("stickyv2");
   }
 }
 
